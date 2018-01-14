4d784bb196d418b7b0075713323149f0683c8beb
Steve Wong <ahwong5@gmail.com>
2018-01-14 13:50:12 +0800

    Update 20160927112210_add_active_solidus_sales_to_spree_promotions.rb

--- db/migrate/20160927112210_add_active_solidus_sales_to_spree_promotions.rb
+++ db/migrate/20160927112210_add_active_solidus_sales_to_spree_promotions.rb
@@ -1,4 +1,4 @@
-class AddActiveSolidusSalesToSpreePromotions < ActiveRecord::Migration
+class AddActiveSolidusSalesToSpreePromotions < ActiveRecord::Migration[5.1]
   def change
     add_column :spree_promotions, :active_solidus_sales, :boolean, default: true
   end
